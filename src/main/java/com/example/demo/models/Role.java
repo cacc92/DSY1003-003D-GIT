ae6fabf672a44bbdd7ddb69948f81f2e8568985f
Cesar <ccarrascocarre@gmail.com>
2025-04-21 12:00:20 -0400

    Se generar clases para trabajar y ejemplo

---
+++ src/main/java/com/example/demo/models/Role.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+public class Role {
+
+    public void algo(){
+        Usuario user = new Usuario();
+        user.getId();
+        user.getName();
+    }
+
+}
