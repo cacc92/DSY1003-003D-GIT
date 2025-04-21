b335b0401892488fe36842e5a7e8e61205e5428f
valparaiso <marc.contrerasa@duocuc.cl>
2025-04-21 11:53:29 -0400

    tranforma clase entidad

--- src/main/java/com/example/demo/models/Usuario.java
+++ src/main/java/com/example/demo/models/Usuario.java
@@ -1,4 +1,15 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
